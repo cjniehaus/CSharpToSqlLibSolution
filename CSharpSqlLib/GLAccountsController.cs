399ba9ffecafa16a589bbe9833eca0f5a877d807
Casey Niehaus <cjniehaus.cn@gmail.com>
2022-01-22 14:47:22 -0500

    Initial commit

---
+++ CSharpSqlLib/GLAccountsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToSqlLib {
+    
+    public class GLAccountsController {
+
+        public Connection Connection { get; set; }
+
+        public bool Delete(GLAccount gla) {
+            var Sql = $" DELETE GLAccounts " +
+                        $" Where AccountNo = @AccountNo; ";
+            var SqlCmd = new SqlCommand(Sql, Connection.SqlConnection);
+            SqlCmd.Parameters.AddWithValue("@AccountNo", gla.AccountNo);
+            var rowsAffected = SqlCmd.ExecuteNonQuery();
+            if (rowsAffected != 1) {
+                throw new Exception("Delete failed");
+            }
+            return true;
+        }
+
+        public bool Update(GLAccount gla) {
+            var Sql = $" UPDATE GLAccounts SET " +
+                        $" AccountDescription = @AccountDescription " +
+                        $" Where AccountNo = @AccountNo; ";
+            var SqlCmd = new SqlCommand(Sql, Connection.SqlConnection);
+            SqlCmd.Parameters.AddWithValue("@AccountNo", gla.AccountNo);
+            SqlCmd.Parameters.AddWithValue("@AccountDescription", gla.AccountDescription);
+            var rowsAffected = SqlCmd.ExecuteNonQuery();
+            if (rowsAffected != 1) {
+                throw new Exception("Update failed");
+            }
+            return true;
+        }
+
+
+        public bool Insert(GLAccount gla) {
+            var Sql = $" INSERT GLAccounts " +
+                        $" (AccountNo, AccountDescription) " +
+                        $" VALUES " +
+                        $" (@AccountNo, @AccountDescription);";
+            var SqlCmd = new SqlCommand(Sql, Connection.SqlConnection);
+            SqlCmd.Parameters.AddWithValue("@AccountNo", gla.AccountNo);
+            SqlCmd.Parameters.AddWithValue("@AccountDescription", gla.AccountDescription);
+            var rowsAffected = SqlCmd.ExecuteNonQuery();
+            if(rowsAffected != 1) {
+                throw new Exception("Insert failed");
+            }
+            return true;
+        }
+
+        public GLAccount Get(int Id) {
+            var Sql = $"SELECT * From GLAccounts Where AccountNo = {Id}";
+            var SqlCmd = new SqlCommand(Sql, Connection.SqlConnection);
+            var reader = SqlCmd.ExecuteReader();
+            if (!reader.HasRows) { 
+                reader.Close();
+                return null;
+            }
+            reader.Read();
+            var gla = new GLAccount();
+            gla.AccountNo = Convert.ToInt32(reader["AccountNo"]);
+            gla.AccountDescription = reader["AccountDescription"].ToString();
+            reader.Close();
+            return gla;
+        }
+
+        public List<GLAccount> List() {
+            var Sql = "SELECT * From GLAccounts;";
+            var SqlCmd = new SqlCommand(Sql, Connection.SqlConnection);
+            var reader = SqlCmd.ExecuteReader();
+            var glas = new List<GLAccount>();
+            while (reader.Read()) { 
+                var gla = new GLAccount();
+                gla.AccountNo = Convert.ToInt32(reader["AccountNo"]);
+                gla.AccountDescription = reader["AccountDescription"].ToString();
+                glas.Add(gla);
+            }
+            reader.Close();
+            return glas;
+        }
+    }
+}
